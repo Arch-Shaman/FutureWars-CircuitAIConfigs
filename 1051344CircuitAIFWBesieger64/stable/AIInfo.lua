815e1aa2ff7f027b7763a8f0c88a79f716319141
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-12-02 10:48:08 -0500

    Actually structure this correctly

--- 1051344CircuitAIFWBesieger64/stable/AIInfo.lua
+++ 1051344CircuitAIFWBesieger64/stable/AIInfo.lua
@@ -16,7 +16,7 @@
 local infos = {
 	{
 		key    = 'shortName',
-		value  = '1051344CircuitAIBrutal64', -- AI name - !This comment is used for parsing!
+		value  = '1051344CircuitAIFWBesieger64', -- AI name - !This comment is used for parsing!
 		desc   = 'machine conform name.',
 	},
 	{
