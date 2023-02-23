58f5847ab8ef6b81de12a1ef466d1bd9b94f9d02
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2023-02-23 07:24:42 -0500

    Name brutal support

--- BrutalCircuitAISupport/stable/AIInfo.lua
+++ BrutalCircuitAISupport/stable/AIInfo.lua
@@ -16,7 +16,7 @@
 local infos = {
 	{
 		key    = 'shortName',
-		value  = 'DevCircuitTest64', -- AI name - !This comment is used for parsing!
+		value  = 'BrutalCircuitAISupport', -- AI name - !This comment is used for parsing!
 		desc   = 'machine conform name.',
 	},
 	{
@@ -25,7 +25,7 @@ local infos = {
 	},
 	{
 		key    = 'name',
-		value  = 'Circuit Test',
+		value  = 'Brutal AI - Support',
 		desc   = 'human readable name.',
 	},
 	{
